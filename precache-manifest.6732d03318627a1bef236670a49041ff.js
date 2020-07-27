0a177160d2b2c67f84ca85c79bce4897f9251bed
Bi <linhnguyen93x@gmail.com>
2020-07-27 23:27:25 +0700

    Updates

---
+++ precache-manifest.6732d03318627a1bef236670a49041ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba1fccfbc92fe134d7b735aa737aa5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aebf5e0f530d4c3663f",
+    "url": "/static/css/main.273efbe9.chunk.css"
+  },
+  {
+    "revision": "ee514fdd97f7c0c1c0d5",
+    "url": "/static/js/2.9374f708.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9374f708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aebf5e0f530d4c3663f",
+    "url": "/static/js/main.9686326a.chunk.js"
+  },
+  {
+    "revision": "ab11b3b943c7b307e64e",
+    "url": "/static/js/runtime-main.21d63445.js"
+  },
+  {
+    "revision": "a6f80b0ac6ff7c05b9ef24dbb7cb2b44",
+    "url": "/static/media/cake.a6f80b0a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thi-bakery/precache-manifest.a4d1bbfebf9e5eba89fa148d7f040d7d.js"
+  "/precache-manifest.6732d03318627a1bef236670a49041ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thi-bakery/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
