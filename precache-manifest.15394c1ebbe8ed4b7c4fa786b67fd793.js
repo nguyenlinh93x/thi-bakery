8cb19e4faba1fd108fa19b888057d8a2bf38f2c9
Bi <linhnguyen93x@gmail.com>
2020-07-27 23:29:12 +0700

    Updates

---
+++ precache-manifest.15394c1ebbe8ed4b7c4fa786b67fd793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fca4e85b8be48d66c452faa4af9b9e7",
+    "url": "/thi-bakery/index.html"
+  },
+  {
+    "revision": "e257a3cfaf58145fb3a0",
+    "url": "/thi-bakery/static/css/main.0febcdc8.chunk.css"
+  },
+  {
+    "revision": "526120fcc89370f84e12",
+    "url": "/thi-bakery/static/js/2.e66e6e72.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/thi-bakery/static/js/2.e66e6e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e257a3cfaf58145fb3a0",
+    "url": "/thi-bakery/static/js/main.60669263.chunk.js"
+  },
+  {
+    "revision": "9d79a2851d77fb5295b5",
+    "url": "/thi-bakery/static/js/runtime-main.8964c7c6.js"
+  },
+  {
+    "revision": "a6f80b0ac6ff7c05b9ef24dbb7cb2b44",
+    "url": "/thi-bakery/static/media/cake.a6f80b0a.jpg"
+  }
+]);
